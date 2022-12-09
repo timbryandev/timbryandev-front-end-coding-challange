828cbcfa302b578c15a4dd10a95c78f6a7437094
Tim Bryan <tim.bryan@drpgroup.com>
2022-12-09 16:10:02 +0000

    [ally] escape key closes modals

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, useEffect } from 'react'
 import Context from '../../store/Context'
 import { ContextState } from '../../store/types'
 import './Modal.css'
@@ -19,12 +19,35 @@ function Modal (): JSX.Element | null {
 
   if (!visible) return null
 
+  useEffect(() => {
+    const closeModalOnEscKey = (evt: KeyboardEvent): void => {
+      if (evt.key === 'Escape') {
+        setContext((prev: ContextState) => ({
+          ...prev,
+          modal: {
+            ...prev.modal,
+            visible: false
+          }
+        }))
+      }
+    }
+
+    document.addEventListener('keyup', closeModalOnEscKey)
+
+    return () => {
+      document.removeEventListener('keyup', closeModalOnEscKey)
+    }
+  }, [])
+
   return (
     <div className='modal'>
       <div className='modal__content'>
         <header className='modal__header'>
           {typeof title === 'string' && <h2>{title}</h2>}
-          <button onClick={handleClose} className="modal__close modal__close--header">
+          <button
+            onClick={handleClose}
+            className='modal__close modal__close--header'
+          >
             &times;
           </button>
         </header>
