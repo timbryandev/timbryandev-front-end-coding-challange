e50511dd39fc4d055e4237481fe206f932f2fcbe
Tim Bryan <tim.bryan@drpgroup.com>
2022-12-09 09:22:42 +0000

    usertable to take users from props

--- src/components/UserTable/UserTable.tsx
+++ src/components/UserTable/UserTable.tsx
@@ -2,9 +2,13 @@ import { UserTableProps } from './types'
 import './UserTable.css'
 
 function UserTable ({ users }: UserTableProps): JSX.Element {
+  function handleEditUser (id: number): void {
+    console.log('TODO: Implement handleEditUser', { id })
+  }
+
   return (
-    <table className="users">
-      <thead className="users__head">
+    <table className='users'>
+      <thead className='users__head'>
         <tr>
           <th>Avatar</th>
           <th>First name</th>
@@ -13,42 +17,34 @@ function UserTable ({ users }: UserTableProps): JSX.Element {
           <th>Action</th>
         </tr>
       </thead>
-      <tbody className="users__body">
-        <tr>
-          <td>image</td>
-          <td>Michael</td>
-          <td>Lawson</td>
-          <td>
-            <a href='mailto:'>michael.lawson@reqres.in</a>
-          </td>
-          <td>Edit Details</td>
-        </tr>
-        <tr>
-          <td>image</td>
-          <td>Lindsay</td>
-          <td>Ferguson</td>
-          <td>
-            <a href='mailto:'>lindsay.ferguson@reqres.in</a>
-          </td>
-          <td>Edit Details</td>
-        </tr>
-        <tr>
-          <td>image</td>
-          <td>Tobias</td>
-          <td>Funke</td>
-          <td>
-            <a href='mailto:'>tobias.funke@reqres.in</a>
-          </td>
-          <td>Edit Details</td>
-        </tr>
-        <tr>
-          <td>…</td>
-          <td>…</td>
-          <td>…</td>
-          <td>…</td>
-          <td></td>
-        </tr>
+      <tbody className='users__body'>
+        {users.map(({ avatar, email, firstName, id, lastName }) => (
+          <tr key={email}>
+            <td>
+              <img
+                className='users__avatar'
+                src={avatar}
+                alt={`Avatar of ${firstName} ${lastName}`}
+              />
+            </td>
+            <td>{firstName}</td>
+            <td>{lastName}</td>
+            <td>
+              <a href={`mailto:${email}`}>{email}</a>
+            </td>
+            <td>
+              <button onClick={() => handleEditUser(id)}>Edit Details</button>
+            </td>
+          </tr>
+        ))}
       </tbody>
+      <tfoot>
+        <tr>
+          <td colSpan={5}>
+            <p>[pagination will go here]</p>
+          </td>
+        </tr>
+      </tfoot>
     </table>
   )
 }
