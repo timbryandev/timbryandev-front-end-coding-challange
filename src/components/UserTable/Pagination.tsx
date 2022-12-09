9dc4a18c9203ab8137fc651aee442003361037ce
Tim Bryan <tim.bryan@drpgroup.com>
2022-12-09 16:49:55 +0000

    pagination now updates context value

--- src/components/UserTable/Pagination.tsx
+++ src/components/UserTable/Pagination.tsx
@@ -1,12 +1,30 @@
+import { useContext } from 'react'
+import Context from '../../store/Context'
 import './Pagination.css'
 
 function Pagination (): JSX.Element {
+  const { context, setContext } = useContext(Context)
+  const { pagination } = context
+
+  const updatePagination = (value: number): void => {
+    setContext({ ...context, pagination: context.pagination + value })
+  }
+
   return (
     <div className='pagination'>
-      <button className='pagination__button' title='previous'>
+      <button
+        className='pagination__button'
+        title='previous'
+        disabled={pagination <= 1}
+        onClick={() => updatePagination(-1)}
+      >
         &lt;
       </button>
-      <button className='pagination__button' title='next'>
+      <button
+        className='pagination__button'
+        title='next'
+        onClick={() => updatePagination(1)}
+      >
         &gt;
       </button>
     </div>
